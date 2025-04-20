5d6522e25c8481e7726d74729bf2e6b498ca0c9b
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-04-20 18:42:37 +0530

    Add LinkedList example method to Collection class

--- Collection.java
+++ Collection.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Collection {
@@ -16,7 +17,22 @@ public class Collection {
         System.out.println(li);
 
     }
+
+    static void LinkedListExample(){
+        List<Integer> li = new LinkedList<>();
+
+        li.add(1);
+        li.add(2);
+        li.add(1);
+
+        System.out.println(li.contains(5));
+        System.out.println(li.indexOf(1));
+        System.out.println(li.get(1));
+        System.out.println(li);
+
+    }
     public static void main(String[] args) {
-        ArrayListExample();
+        // ArrayListExample();
+        LinkedListExample();
     }
 }
